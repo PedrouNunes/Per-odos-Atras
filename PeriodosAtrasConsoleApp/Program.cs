ad20803e8ce7824a8a1e89f9393d74a0adae8c0f
PedrouNunes <90865544+PedrouNunes@users.noreply.github.com>
2022-03-24 16:00:54 -0300

    Add project files.

---
+++ PeriodosAtrasConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PeriodosAtrasConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma data: ");
+            string data = Console.ReadLine();
+
+            DateTime dataDoDia = Convert.ToDateTime(data);
+
+            TimeSpan diasDeDiferenca = DateTime.Now - dataDoDia;
+
+            double segundos = diasDeDiferenca.TotalSeconds;
+            double horas = diasDeDiferenca.TotalHours;
+            double minutos = diasDeDiferenca.TotalMinutes;
+            double dias = diasDeDiferenca.TotalDays;
+         
+
+
+
+            Console.WriteLine("dias: " + dias + ", horas: " + horas + ", minutos: " + minutos + ", segundos: " + segundos);
+
+
+        }
+    }
+}
